7b5af51ddadf7f4abc709ed32a947b45baab54c2
do.kim <do.kim@krafton.com>
2023-07-06 13:26:57 +0900

    add oidc document code

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/zitadel/oidc/v2/pkg/oidc"
+	"github.com/zitadel/oidc/v2/pkg/op"
+	"net/http"
+	"path"
+)
+
+type KeyProvider struct {
+}
+
+func (provider *KeyProvider) KeySet(ctx context.Context) ([]op.Key, error) {
+	return nil, nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	issuer := "https://localhost:8080/"
+
+	discoveryConf := oidc.DiscoveryConfiguration{
+		Issuer:                           issuer,
+		JwksURI:                          path.Join(issuer, "keys"),
+		IDTokenSigningAlgValuesSupported: []string{"RS256"},
+	}
+
+	keyProvider := KeyProvider{}
+
+	http.HandleFunc("/.well-known/openid-configuration", func(w http.ResponseWriter, r *http.Request) {
+		op.Discover(w, &discoveryConf)
+	})
+
+	http.HandleFunc("/keys", func(w http.ResponseWriter, r *http.Request) {
+		op.Keys(w, r, &keyProvider)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
