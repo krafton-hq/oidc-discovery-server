c88e83daf0d877225982016d9c1863359777e985
do.kim <do.kim@krafton.com>
2023-07-31 14:49:54 +0900

    fix k8s jwks request rate limiting (#12)
    fix unwanted arguments

--- key_provider/k8s.go
+++ key_provider/k8s.go
@@ -7,11 +7,14 @@ import (
 	"github.krafton.com/sbx/oidc-discovery-server/jwt"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"time"
 )
 
 // TODO: out-cluster support?
 type K8SKeyProvider struct {
-	client *kubernetes.Clientset
+	client  *kubernetes.Clientset
+	keys    []op.Key
+	expires time.Time
 }
 
 func NewK8SKeyProvider() (*K8SKeyProvider, error) {
@@ -25,18 +28,33 @@ func NewK8SKeyProvider() (*K8SKeyProvider, error) {
 		return nil, err
 	}
 
-	return &K8SKeyProvider{client: clientSet}, nil
+	return &K8SKeyProvider{
+		clientSet,
+		nil,
+		time.Now(),
+	}, nil
 }
 
 func (provider *K8SKeyProvider) KeySet(ctx context.Context) ([]op.Key, error) {
+	if provider.Expires(time.Now()) {
+		err := provider.update(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return provider.keys, nil
+}
+
+func (provider *K8SKeyProvider) update(ctx context.Context) error {
 	body, err := provider.client.RESTClient().Get().AbsPath("/openid/v1/jwks").DoRaw(ctx)
 	if err != nil {
-		return nil, errors.Wrap(err, "error while getting jwks from k8s in-cluster")
+		return errors.Wrap(err, "error while getting jwks from k8s in-cluster")
 	}
 
 	keys, err := jwt.ParseJWKS(body)
 	if err != nil {
-		return nil, errors.Wrap(err, "error while parsing jwks from k8s in-cluster")
+		return errors.Wrap(err, "error while parsing jwks from k8s in-cluster")
 	}
 
 	keys2 := make([]op.Key, len(keys))
@@ -44,5 +62,12 @@ func (provider *K8SKeyProvider) KeySet(ctx context.Context) ([]op.Key, error) {
 		keys2[i] = &jwt.JsonWebKey{JSONWebKey: key}
 	}
 
-	return keys2, nil
+	provider.keys = keys2
+	provider.expires = time.Now().Add(60 * time.Second)
+
+	return nil
+}
+
+func (provider *K8SKeyProvider) Expires(now time.Time) bool {
+	return provider.expires.Before(now)
 }
