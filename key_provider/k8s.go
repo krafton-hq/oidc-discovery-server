7657ce99f5244e2368f31035d13ad188e9b96252
do.kim <do.kim@krafton.com>
2023-07-31 14:47:07 +0900

    fix k8s rate limiting

--- key_provider/k8s.go
+++ key_provider/k8s.go
@@ -7,14 +7,18 @@ import (
 	"github.krafton.com/sbx/oidc-discovery-server/jwt"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"time"
 )
 
 // TODO: out-cluster support?
 type K8SKeyProvider struct {
-	client *kubernetes.Clientset
+	client  *kubernetes.Clientset
+	keys    []op.Key
+	expires time.Time
+	keyTTL  time.Duration
 }
 
-func NewK8SKeyProvider() (*K8SKeyProvider, error) {
+func NewK8SKeyProvider(keyTTL time.Duration) (*K8SKeyProvider, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		return nil, errors.Wrap(err, "error while getting in-cluster config")
@@ -25,18 +29,34 @@ func NewK8SKeyProvider() (*K8SKeyProvider, error) {
 		return nil, err
 	}
 
-	return &K8SKeyProvider{client: clientSet}, nil
+	return &K8SKeyProvider{
+		clientSet,
+		nil,
+		time.Now(),
+		keyTTL,
+	}, nil
 }
 
 func (provider *K8SKeyProvider) KeySet(ctx context.Context) ([]op.Key, error) {
+	if provider.Expires(time.Now()) {
+		err := provider.update(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return provider.keys, nil
+}
+
+func (provider *K8SKeyProvider) update(ctx context.Context) error {
 	body, err := provider.client.RESTClient().Get().AbsPath("/openid/v1/jwks").DoRaw(ctx)
 	if err != nil {
-		return nil, errors.Wrap(err, "error while getting jwks from k8s in-cluster")
+		return errors.Wrap(err, "error while getting jwks from k8s in-cluster")
 	}
 
 	keys, err := jwt.ParseJWKS(body)
 	if err != nil {
-		return nil, errors.Wrap(err, "error while parsing jwks from k8s in-cluster")
+		return errors.Wrap(err, "error while parsing jwks from k8s in-cluster")
 	}
 
 	keys2 := make([]op.Key, len(keys))
@@ -44,5 +64,12 @@ func (provider *K8SKeyProvider) KeySet(ctx context.Context) ([]op.Key, error) {
 		keys2[i] = &jwt.JsonWebKey{JSONWebKey: key}
 	}
 
-	return keys2, nil
+	provider.keys = keys2
+	provider.expires = time.Now().Add(60 * time.Second)
+
+	return nil
+}
+
+func (provider *K8SKeyProvider) Expires(now time.Time) bool {
+	return provider.expires.Before(now)
 }
